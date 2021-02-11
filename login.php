9f489c0e3d5be7acb97728940f5b5b245153ee92
Drodyaye92 <aichanana24@gmail.com>
2021-02-11 18:09:09 +0000

    third commit 10fev

--- login.php
+++ login.php
@@ -4,8 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href=" styles/style.css">
+    <link rel="stylesheet" href=" bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="style/app.js">
+
 </head>
 <body>
-    
+   <section class="gauche">
+   <aside class="cote"> 
+       <div class="img"> 
+           <img src="images/lacsoftforum.png">
+      </div>
+   <div>
+       <h3><button type="submit" class="btn btn-primary">log in</button></h3>
+       <h3><button type="submit" class="btn btn-primary">sign up</button></h3>
+       <h3>Login now</h3>
+   </div>
+    <form class="radus">
+  <div class="form-group">
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">       
+  </div>
+  <div class="form-group">
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              <small id="emailHelp" class="form-text text-muted">forgot password?</small>
+  </div>
+  <button type="submit" class="btn btn-primary">login</button>
+</form>
+   </aside>
+   </section> 
+   <section class="droite">
+       <h1>Lac Soft Forum</h1>
+       <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet doloremque nostrum asperiores aliquam inventore autem odio, ipsum nihil, ipsam ea ipsa earum neque magni quas sapiente velit. Harum, suscipit eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam facere neque, animi et vero provident id unde aliquid ad cupiditate sequi nobis autem iure sit facilis excepturi, laborum quas tenetur?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat autem quia inventore cum dolores officia fuga, quam repellendus voluptates neque! Aut libero praesentium illum. Nemo dolores distinctio saepe enim possimus?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur id laboriosam accusamus repellendus reprehenderit provident quidem? Ea a itaque, nostrum assumenda perspiciatis facilis! Natus dolore veniam repellat suscipit quisquam dolores.</h4>
+   </section>
 </body>
 </html>
